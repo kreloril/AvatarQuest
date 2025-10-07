451a21acbd613deb47621d6d6be7d588bdc4bd81
jcmulvey <jcmulvey@gmail.com>
2025-10-07 15:31:38 -0700

    Make AvatarQuestLayer react to Escape via Game::endGameLoop; call handleEvents from update

--- src/AvatarQuest/AvatarQuestLayer.cpp
+++ src/AvatarQuest/AvatarQuestLayer.cpp
@@ -9,7 +9,22 @@ void AvatarQuestLayer::init() {}
 void AvatarQuestLayer::update(float /*deltaTime*/) {}
 void AvatarQuestLayer::render(float /*deltaTime*/) {}
 void AvatarQuestLayer::shutDown() {}
-void AvatarQuestLayer::handleEvents(float /*delta*/, Game::GameEvents& /*events*/) {}
+
+void AvatarQuestLayer::handleEvents(float /*delta*/, Game::GameEvents& events) {
+    using Game::GameEvents;
+    if (events.isEventType(GameEvents::EventType::KeyPress)) {
+        // SDL keycodes are forwarded in keyEvent.keyCode
+        if (events.keyEvent.keyCode == SDLK_ESCAPE) {
+            Game::endGameLoop();
+        }
+    }
+}
+
+// Ensure we call the event handler from update so layer can react to events
+void AvatarQuestLayer::update(float deltaTime) {
+    Game::GameEvents& events = Game::getGameEvents();
+    handleEvents(deltaTime, events);
+}
 
 Ref<Game::UILayer> CreateAvatarQuestLayer() {
     return CreateRef<AvatarQuestLayer>();
